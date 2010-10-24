9a09831c9eaa25f2435a0d68ac75edead7b93d37
leandro.mattioli <leandro.mattioli@143e74d3-472b-d0de-8f1e-12dc55512020>
2010-10-24 13:10:42 +0000

    Transfer to Google Code.

---
+++ trunk/pkgcreator/PkgCreator/icon_creator.py
@@ -0,0 +1,5 @@
+PIXMAP = 'usr/share/%s/pixmaps/%s.xpm'
+PNG = '/usr/share/icons/hicolor/%s/apps/%s.png'
+
+class IconCreator(object)
+    pass
---
+++ trunk/pkgcreator/PkgCreator/menu_creator.py
@@ -0,0 +1,8 @@
+APP_REG_ENTRY_PATH = 'usr/share/application-registry/%s.application'
+APP_ENTRY_PATH = 'usr/share/applications/%s.desktop'
+MENU_ENTRY = 'usr/share/menu/%s'
+
+class MenuCreator(object):
+    def __init__(self, info):
+        self.info = info
+    def

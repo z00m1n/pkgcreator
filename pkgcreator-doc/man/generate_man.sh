664372f232ad0ebbaf582a4e3c6d6edc1ebf9d35
leandro.mattioli <leandro.mattioli@143e74d3-472b-d0de-8f1e-12dc55512020>
2011-03-01 15:06:59 +0000

    Documentation output added.

--- pkgcreator-doc/man/generate_man.sh
+++ pkgcreator-doc/man/generate_man.sh
@@ -2,4 +2,4 @@
 #nroff -man pkgcreator_nroff.1 > pkgcreator.1 --> MAKES FILE INVALID!
 cp pkgcreator_nroff.1 pkgcreator.1
 gzip -9 pkgcreator.1
-mv pkgcreator.1.gz ../pkgcreator/man/
+mv pkgcreator.1.gz ../../pkgcreator/man/
